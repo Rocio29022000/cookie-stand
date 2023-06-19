aeb3bd73eb74547445e923785491c23dbe7eb91f
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-06-19 15:39:46 +0100

    added render function and added loop to create random numbers for customers

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const parentElement = document.getElementById("shops")
-console.log(parentElement)
 
 //Hours
 const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
@@ -11,12 +10,52 @@ const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm",
 const seattle = {
     storeName: "Seattle",
     minCustPerHour: 0,
-    maxCustPerHour: 0,
-    avgCookiesPerCust: 0,
-    customersEachHour: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-    cookiesEachHour: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57],
-    totalDailyCookies: 875,
-    // totalDailyCookies: function(){
+    maxCustPerHour: 30,
+    avgCookiesPerCust: 6.3,
+    customersEachHour: [],
+    cookiesEachHour: [],
+    totalDailyCookies: 0,
+    calcCustomersEachHour: function(){
+        for (let i = 0; i < hours.length; i++ ){
+            this.customersEachHour.push(randomCust(this.minCustPerHour, this.maxCustPerHour)[i])
+        }
+    },
+    calcCookiesEachHour: function(){
+        
+    },
+    render: function(){
+        const article = document.createElement("article");
+        parentElement.appendChild(article);
+        const h3 = document.createElement("h3");
+        h3.textContent = seattle.storeName;
+        article.appendChild(h3);
+        const ul = document.createElement("ul");
+        article.appendChild(ul);
+        for (let i = 0; i < hours.length; i++ ){  
+            const li = document.createElement("li")
+            li.textContent = ` ${hours[i]}  ${seattle.cookiesEachHour[i]} cookies `
+            ul.appendChild(li)
+        }
+    }
+    
+    
+}
+
+//function to generate random customers
+function randomCust(min,max){
+    let randCustNum = []
+    for (let i = 0; i < hours.length; i++){
+        randCustNum[i] = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    return randCustNum;
+
+}
+
+console.log(randomCust(seattle.minCustPerHour, seattle.maxCustPerHour))
+
+// console.log(seattle.totalDailyCookies())
+
+// totalDailyCookies: function(){
     //     let totalDailyCookies = 0
     //     console.log(this.cookiesEachHour.length)
     //     for (let i = 0; i < this.cookiesEachHour.length; i++){
@@ -24,34 +63,3 @@ const seattle = {
     //     }
     //     return totalDailyCookies
     // }
-
-}
-
-console.log(seattle.minCustPerHour)
-
-// console.log(seattle.totalDailyCookies())
-
-//DOM manipulation
-
-//article - as a container
-
-const article = document.createElement("article");
-parentElement.appendChild(article);
-
-// h3 - store title
-
-const h3 = document.createElement("h3");
-h3.textContent = "Seattle";
-article.appendChild(h3);
-
-// ul for the list of hours and cookies sold
-
-const ul = document.createElement("ul");
-article.appendChild(ul);
-
-// a loop to create each list item
-for (let i = 0; i < hours.length; i++ ){  
-    const li = document.createElement("li")
-    li.textContent = ` ${hours[i]}  ${seattle.cookiesEachHour[i]} cookies `
-    ul.appendChild(li)
-}
