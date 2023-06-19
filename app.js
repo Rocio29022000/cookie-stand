62edcd758071c8f0493a22add23d4b77e3d2913c
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-06-19 13:57:31 +0100

    Created the seattle object with the key value pairs

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 "use strict";
 
+const parentElement = document.getElementById("shops")
+console.log(parentElement)
+
+
 //Create objet for First shop: Seattle
 
+const seattle = {
+    minCustPerHour: 0,
+    maxCustPerHour: 0,
+    avgCookiesPerCust: 0,
+    customersEachHour: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    cookiesEachHour: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    totalDailyCookies: function(){
+        let totalDailyCookies = 0
+        console.log(this.cookiesEachHour.length)
+        for (let i = 0; i < this.cookiesEachHour.length; i++){
+            totalDailyCookies += this.cookiesEachHour[i];
+        }
+        return totalDailyCookies
+    }
+
+}
+
+console.log(seattle.minCustPerHour)
+
+console.log(seattle.totalDailyCookies())
+
+
+
+// function sumDailyCookies(){
+    
+// }
