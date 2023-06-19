bb925d681cd018ac86fa536ca482721d164634e8
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-06-19 16:25:44 +0100

    Update

--- app.js
+++ app.js
@@ -9,31 +9,33 @@ const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm",
 
 const seattle = {
     storeName: "Seattle",
-    minCustPerHour: 0,
-    maxCustPerHour: 30,
+    minCustPerHour: 23,
+    maxCustPerHour: 65,
     avgCookiesPerCust: 6.3,
     customersEachHour: [],
     cookiesEachHour: [],
     totalDailyCookies: 0,
     calcCustomersEachHour: function(){
         for (let i = 0; i < hours.length; i++ ){
-            this.customersEachHour.push(randomCust(this.minCustPerHour, this.maxCustPerHour)[i])
+            this.customersEachHour.push(randomCust(this.minCustPerHour, this.maxCustPerHour))
         }
+        console.log(this.customersEachHour)
     },
     calcCookiesEachHour: function(){
-        
+        console.log("hi")
     },
     render: function(){
+        this.calcCustomersEachHour()
         const article = document.createElement("article");
         parentElement.appendChild(article);
         const h3 = document.createElement("h3");
-        h3.textContent = seattle.storeName;
+        h3.textContent = this.storeName;
         article.appendChild(h3);
         const ul = document.createElement("ul");
         article.appendChild(ul);
         for (let i = 0; i < hours.length; i++ ){  
             const li = document.createElement("li")
-            li.textContent = ` ${hours[i]}  ${seattle.cookiesEachHour[i]} cookies `
+            li.textContent = ` ${hours[i]}  ${this.cookiesEachHour[i]} cookies `
             ul.appendChild(li)
         }
     }
@@ -41,17 +43,20 @@ const seattle = {
     
 }
 
+seattle.render()
+
 //function to generate random customers
 function randomCust(min,max){
-    let randCustNum = []
-    for (let i = 0; i < hours.length; i++){
-        randCustNum[i] = Math.floor(Math.random() * (max - min + 1) + min);
-    }
-    return randCustNum;
+    return Math.floor(Math.random()* (max - min + 1 ) + min)
+    // let randCustNum = []
+    // for (let i = 0; i < hours.length; i++){
+    //     randCustNum[i] = Math.floor(Math.random() * (max - min + 1) + min);
+    // }
+    // return randCustNum;
 
 }
 
-console.log(randomCust(seattle.minCustPerHour, seattle.maxCustPerHour))
+// console.log(randomCust(seattle.minCustPerHour, seattle.maxCustPerHour))
 
 // console.log(seattle.totalDailyCookies())
 
